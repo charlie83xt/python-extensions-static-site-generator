125b679fcd2e196cd90c306b3a525047b086d6df
charlie83xt <65869775+charlie83xt@users.noreply.github.com>
2023-01-05 15:09:15 +0000

    load extensions 2

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -3,7 +3,7 @@ import importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.insert(directory)[0]
+    sys.path.insert(directory)
 
 def load_directory(directory):
     for path in directory:
